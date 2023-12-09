e491ce97b160ffae5d9f7198ad9977ca2ba96f43
Vondi544 <vondijuanda1@gmail.com>
2023-12-09 14:09:04 +0700

    first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Hello_word"
+include(":app")
+ 
